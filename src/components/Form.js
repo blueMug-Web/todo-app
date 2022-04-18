f9d6ab367e797434f474e30d1b945b4ae9834446
blueMug <wrightman.kyle@gmail.com>
2022-04-18 00:09:48 -0400

    adjusted mobile layout

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,41 +1,51 @@
-import React from 'react'
+import React from "react";
 
-const Form = ({inputText, setInputText, todos, setToDos, setStatus}) => {
+const Form = ({ inputText, setInputText, todos, setToDos, setStatus }) => {
+	const inputTextHandler = (e) => {
+		setInputText(e.target.value);
+	};
+	const submitTodoHandler = (e) => {
+		e.preventDefault();
+		setToDos([
+			...todos, //pass in whatever is already in todos list(array)
+			{
+				text: inputText,
+				completed: false,
+				id: Math.random() * 1000, //For real production, use a package to generate actual id's (this method just for example)
+			},
+		]);
+		setInputText("");
+	};
+	const statusHandler = (e) => {
+		setStatus(e.target.value);
+	};
 
-  const inputTextHandler = (e) => {
-    setInputText(e.target.value);
-  };
-  const submitTodoHandler = (e) => {
-    e.preventDefault();
-    setToDos([
-      ...todos, //pass in whatever is already in todos list(array)
-      {
-        text: inputText,
-        completed: false,
-        id: Math.random() * 1000 //For real production, use a package to generate actual id's (this method just for example)
-      }
-    ]);
-    setInputText("");
-  };
-  const statusHandler = (e) => {
-    setStatus(e.target.value);
-  }
+	return (
+		<div className="form-container">
+			<form>
+				<input
+					value={inputText}
+					onChange={inputTextHandler}
+					type="text"
+					classNameName="todo-input"
+				/>
+				<button
+					onClick={submitTodoHandler}
+					className="todo-button"
+					type="submit"
+				>
+					<i className="fas fa-plus-square"></i>
+				</button>
+				<div className="select-container">
+					<select onChange={statusHandler} name="todos" className="filter-todo">
+						<option value="all">All</option>
+						<option value="completed">Completed</option>
+						<option value="uncompleted">Uncompleted</option>
+					</select>
+				</div>
+			</form>
+		</div>
+	);
+};
 
-  return (
-    <form>
-      <input value={inputText} onChange={inputTextHandler} type="text" classNameName="todo-input" />
-      <button onClick={submitTodoHandler} className="todo-button" type="submit">
-        <i className="fas fa-plus-square"></i>
-      </button>
-      <div className="select">
-        <select onChange={statusHandler} name="todos" className="filter-todo">
-          <option value="all">All</option>
-          <option value="completed">Completed</option>
-          <option value="uncompleted">Uncompleted</option>
-        </select>
-      </div>
-    </form>
-  );
-}
-
-export default Form
+export default Form;
